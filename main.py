349098e35dd09dd878d8815fb0cc3b53de6b646c
Mahmoud-Yasser-16 <mahmoud.yasser@corporatica.com>
2022-06-18 06:06:15 +0200

    add machine

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from predict import pipeline_audio 
 import librosa
 app = Flask(__name__)
@@ -6,14 +6,15 @@ app = Flask(__name__)
 ### Web Pages ###
 @app.route("/")
 def home():
-    # file_name="a.wav"
-    # y, sr = librosa.load(file_name,sr=None)
-    # print(pipeline_audio(y))
-    return render_template("home.html")
+    file_name="a.wav"
+    y, sr = librosa.load(file_name,sr=None)
+    print(pipeline_audio(y))
+    # return render_template("home.html")
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    pass
+    # return render_template("about.html")
 
 
 
